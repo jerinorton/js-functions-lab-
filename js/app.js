a5a285eb29aec6091223b0e8fa14431c895b3865
JerinO <orton.denise@yahoo.com>
2023-09-27 23:05:28 -0400

    Learned about the return statement

--- js/app.js
+++ js/app.js
@@ -39,3 +39,28 @@ sayGoodbye('Peace', 'Frankie')
 // chao, Becky
 // goodbye, Chad
 // peace, Frankie
+
+
+function add(x, y){
+    const sum = x + y
+    console.log(sum)
+    return sum
+    //return x + y // return produces a value for the function when its called
+}
+console.log(add(2, 2), "<_ _________ LOOK AT THIS")
+
+function getPlayerUserName(username){
+    return username.toUpperCase();
+}
+// before the game we dont know the players name
+let playerOne;
+let playerTwo;
+let playerThree; 
+let playerFour;
+
+//after they join the game we want to set the players name
+
+playerOne = getPlayerUserName('Luke') // == > 'Luke'
+playerTwo = getPlayerUserName('Laura') // == > 'Laura'
+playerThree = getPlayerUserName('Kate') // == > 'Kate'
+playerFour = getPlayerUserName('Kevin') // == > 'Kevin'
