3e349cdf58a8c9ff7ce4223581183e7375d6bd7b
JerinO <orton.denise@yahoo.com>
2023-09-27 22:17:30 -0400

    Learning how functions work, parameters, arguments

---
+++ js/app.js
@@ -0,0 +1,41 @@
+
+console.log('this works!')
+
+// defining a function
+// declaring a function
+// creating the reusable block of code
+// to be used at a later time in the program
+function sayHello(peronsName){
+    // write the code to say hello here
+    console.log(`Hello ${peronsName}`)
+}
+
+// execute the function
+// invoke the function
+// call the function
+
+// write the function name ()
+sayHello('Shavon'); // runs the block code defined by the sayHello func
+sayHello('Mel');
+sayHello('Ed');
+sayHello('Augusta');
+
+
+function sayGoodbye(phrase, personsName){
+console.log(`${phrase}, ${personsName}`)
+}
+
+sayGoodbye('Adios', 'Laura')
+sayGoodbye('Chao', 'Becky')
+sayGoodbye('Goodbye','Chad')
+sayGoodbye('Peace', 'Frankie')
+
+
+
+// define and call the function
+// sayGoodbye should accept two parameters, one phrase, and one person
+// add it logs a message
+// adios, Laura
+// chao, Becky
+// goodbye, Chad
+// peace, Frankie
